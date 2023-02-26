dd51730a03cf66ce8207475cac5b9906d4474fed
AntonRPA <jobtakeranton@mail.ru>
2023-02-26 16:57:04 +0600

    Updates

--- static/js/NotFound.c56a933c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,a,r){r.r(a);r(2791);var t=r(3904),u=r(184);a.default=function(){return(0,u.jsx)(t.vr,{})}}}]);
-//# sourceMappingURL=NotFound.c56a933c.chunk.js.map
